424b6ffefc15bcfb9e790297efc6117956a55e3e
abylaev13 <islannelson@gmail.com>
2021-04-29 16:29:29 +0600

    Hedaer - footer - cards

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,20 @@ import ProductList from "./components/ProductList/ProductList";
 import ProductCard from "./components/ProductCard/ProductCard";
 
 import ProductContextProvider from "./contexts/ProductContext/ProductContext";
+import Header from "./components/Header/Header";
+import Container from "./components/Container/Container";
+import Home from "./components/Home/Home";
+
+
 
 const Routes = () => {
     return (
         <ProductContextProvider>
             <BrowserRouter>
                 <Switch>
+                    <Route exact path="/"    component={Home} />
+                    <Route exact path="/container" component={Container} />
+                    <Route exact path="/header" component={Header} />
                     <Route exact path="/add" component={AddCard} />
                     <Route exact path="/list" component={ProductList} />
                     <Route exact path="/main" component={ProductCard} />
