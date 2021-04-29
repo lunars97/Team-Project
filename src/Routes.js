8500c3afa5be8928a758aaa33fe4e013fd20e6fa
Marat <profileMarat@gmail.com>
2021-04-29 17:22:06 +0600

    blalblabla

--- src/Routes.js
+++ src/Routes.js
@@ -5,17 +5,24 @@ import ProductList from "./components/ProductList/ProductList";
 import ProductCard from "./components/ProductCard/ProductCard";
 
 import ProductContextProvider from "./contexts/ProductContext/ProductContext";
+import AuthContextProvider from "./contexts/AuthContext/AuthContext";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
 const Routes = () => {
     return (
         <ProductContextProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/add" component={AddCard} />
-                    <Route exact path="/list" component={ProductList} />
-                    <Route exact path="/main" component={ProductCard} />
-                </Switch>
-            </BrowserRouter>
+            <AuthContextProvider>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/add" component={AddCard} />
+                        <Route exact path="/list" component={ProductList} />
+                        <Route exact path="/main" component={ProductCard} />
+                        <Route exact path ="/login" component = {SignIn}/>
+                        <Route exact path="/signup" component={SignUp} />
+                    </Switch>
+                </BrowserRouter>
+            </AuthContextProvider>
         </ProductContextProvider>
     );
 };
